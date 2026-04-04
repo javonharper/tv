6aa442d9f11320ddb034ccc390fef4507cab4f03
Javon Harper <javon.d.harper@gmail.com>
2026-04-04 00:19:03 -0400

    Add HTML structure and CSS for channel display

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,15 @@ async fn handler() -> Markup {
 
     let now = Zoned::now().round(Unit::Second).unwrap();
     let date = now.strftime("%A %B %e");
+    let date_short = now.strftime("%b %e");
     let time = now.strftime("%-I:%M%P");
 
     let channels = vec!["Channel 1", "Channel 2", "Channel 3"];
 
     let markup = html! {
+        title { (name) " · " (date_short)}
+        link rel="stylesheet" href="/styles.css" {}
+
         p { (name) " · " (date) " · " (time) }
         div.grid {
             @for channel in channels {
