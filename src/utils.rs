fe7a8b0f28dd4fcd79f8bc2b34bdca1e94b14d48
Javon Harper <javon.d.harper@gmail.com>
2026-07-15 19:48:28 -0400

    Remove dead code

--- src/utils.rs
+++ src/utils.rs
@@ -8,12 +8,6 @@ pub fn current_time_in_minutes() -> i32 {
     hours * 60 + minutes
 }
 
-/// Generates a YouTube search URL for the trailer of a given title
-pub fn trailer_url(title: &str) -> String {
-    let query = title.replace(" ", "+") + "+trailer";
-    format!("https://www.youtube.com/results?search_query={}", query)
-}
-
 /// Generates a watch URL for a given title using the template from environment variables
 /// Returns None if WATCH_URL_TEMPLATE is not defined in the environment
 pub fn watch_url(title: &str) -> Option<String> {
